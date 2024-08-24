6b36287a688dfe5de942e4352718a78934de43f1
Pranav Pokale <pranavpokale50034@gmail.com>
2024-08-24 20:52:07 +0530

    bug fix

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -17,7 +17,7 @@ const Header = () => {
         </div>
         <div className="flex items-center gap-4">
            <div>
-              <DateTime />
+              {/* <DateTime /> */}
            </div>
            <CircleHelp />
            <MessageSquareWarning />
