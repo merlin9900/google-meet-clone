b73a8a76fa6a83a53d9d754d3e8d5269ace68841
Pranav Pokale <pranavpokale50034@gmail.com>
2025-02-21 18:49:03 +0530

    chore: log check

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,9 +1,8 @@
 import { io } from "socket.io-client";
 
+const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "";
 
-// const SOCKET_SERVER_URL = "https://socket.nomineelife.com";
-
-const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || ""
+console.log(SOCKET_SERVER_URL);
 
 const socket = io(SOCKET_SERVER_URL);
 
